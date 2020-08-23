0c9dc3204d74022f833ded84f690a60d81e73496
NTONGA HERVE YVES <56480426+hervegithub@users.noreply.github.com>
2020-08-24 00:39:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/Group-A-Frontend'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Group-A-Frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
